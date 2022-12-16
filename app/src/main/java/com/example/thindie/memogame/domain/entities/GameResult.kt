448d8b08d2297aea68acc5ad7547f63eab85b37d
Alex <thindie1@mail.ru>
2022-12-16 23:10:04 +0200

    alphaVersion Game

--- app/src/main/java/com/example/thindie/memogame/domain/entities/GameResult.kt
+++ app/src/main/java/com/example/thindie/memogame/domain/entities/GameResult.kt
@@ -1,6 +1,10 @@
 package com.example.thindie.memogame.domain.entities
 
+import android.os.Parcelable
+import kotlinx.android.parcel.Parcelize
+
+@Parcelize
 data class GameResult (
     val score : Int,
     val time : Int,
-)
+) : Parcelable
